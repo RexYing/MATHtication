b5d4e03dc29d7b7d0def18d8700d26a39f37f562
RexYing <zy26@duke.edu>
2013-10-06 22:14:09 -0400

    fast marching toolbox

--- data_processing/driver_script.m
+++ data_processing/driver_script.m
@@ -1,3 +1,32 @@
-load_jaws;
+%% Driver script
+% load_jaws (either original or after removing far-away vertices)
+%
+% Find axes
+%
+
+clear all
+close all
+
+data_folder = '../data/';
+
+%% load_jaws
+% The script reads from 2 ply files to give:
+%
+% verts_lower, faces_lower: vertices and faces list of lower jaw mesh
+%
+% verts_upper, faces_upper: vertices and faces list of upper jaw mesh
+
+% Original
+[verts_lower, faces_lower] = load_mesh('lower_cropped-downsampled.ply');
+hold on;
+[verts_upper, faces_upper] = load_mesh('upper_cropped-downsampled.ply');
+
+% after removing far-away vertices
+%load_mesh('lower_cropped-downsampled.ply');
+%load_mesh('upper_cropped-downsampled.ply');
+
+%% Find axes for upper and lower jaws
+
 axes_upper = find_axes(verts_upper);
 axes_lower = find_axes(verts_lower);
+
---
+++ data_processing/load_mesh.m
@@ -0,0 +1,47 @@
+function [ verts, faces ] = load_mesh( filename )
+%%
+%
+% * SYNTAX
+%
+%   [ vertices, faces ] = load_mesh(filename);
+%
+% * INPUT
+%
+% filename    name of the file containing the mesh in ply format
+%
+% * OUTPUT
+%
+% vertices    [N-by-3] matrix representing vertices
+% faces       [N-by-3] matrix representing faces
+%
+% * DESCRIPTION
+%
+% load mesh with "standard options" ie. black-white, smooth.
+% The script reads from ply files to give vertices and faces for the mesh
+% and visualize them.
+%
+% * DEPENDENCIES
+%
+% toolbox_graph by Gabriel Peyré
+%
+% * REFERENCES
+%
+% * AUTHOR
+%
+% Rex Ying
+%
+
+%name = 'lower_cropped-downsampled.ply';
+options.name = filename;
+[verts, faces]=read_mesh(filename);
+options.face_vertex_color = repmat([0.5, 0.5, 0.5], size(verts, 1), 1);
+shading interp;
+plot_mesh(verts, faces, options);
+
+
+% -------------------------------
+% Date : May 10, 2013
+% Rex Ying
+% Duke University
+% Revision :
+% ------------------------------
