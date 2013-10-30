cf47c4d48ca7041927b21d97cb565e356753b453
Rex Ying <zy26@duke.edu>
2013-10-30 03:00:14 -0400

    try rotation for BSRC

--- data_processing/BSRC/find_BSRC.m
+++ data_processing/BSRC/find_BSRC.m
@@ -10,10 +10,10 @@
 
 plane1 = [0, 0, 0, axes_lower_cropped(:, 1)', axes_lower_cropped(:, 2)'];
 plane2 = [0, 0, 0, axes_upper_cropped(:, 2)', axes_upper_cropped(:, 1)'];
-basisTransMat = createBasisTransform3d(plane2, plane1);
+originalBasisTransMat = createBasisTransform3d(plane2, plane1);
 
 disp('transformation beween bases of 2 coordinate systems (upper and lower jaws respectively):');
-basisTransMat = basisTransMat(1:3, 1:3);
+basisTransMat = originalBasisTransMat(1:3, 1:3);
 disp(basisTransMat);
 
 
@@ -34,14 +34,53 @@ disp('Constructing bounding box hierarchy structure...');
 obj_lower = pqp_createmodel(verts_lower', faces_lower');
 obj_upper = pqp_createmodel(verts_upper', faces_upper');
 
+% original
 intersects = pqp_intersect(obj_upper, obj_lower, eye(3), [0; 0; 0], 1);
 fprintf('Number of bad faces: %d\n', length(intersects));
 
+vertical_axis = axes_lower_cropped(:, 3);
+max_ratio = 0.5;
+intersects = pqp_intersect(obj_upper, obj_lower, eye(3), ...
+    vertical_axis * -max_ratio, 1);
+
+% binary search for best
+lower_bound = 0;
+upper_bound = max_ratio;
+
+for i = 1: 10
+    mid = (upper_bound + lower_bound) / 2;
+    
+    intersects = pqp_intersect(obj_upper, obj_lower, eye(3), ...
+        vertical_axis * -mid, 1);
+    if (isempty(intersects))
+        upper_bound = mid;
+    else
+        lower_bound = mid;
+    end
+end
+        
+
+% symmetry plane aligned
 intersects = pqp_intersect(obj_upper, obj_lower, basisTransMat, [0; 0; 0], 1);
 fprintf('Number of bad faces after face alignment: %d\n', length(intersects) - 1000);
 
+vertical_axis = axes_lower_cropped(:, 3);
+max_ratio = 1.5;
+intersects = pqp_intersect(obj_upper, obj_lower, basisTransMat, ...
+    vertical_axis * -max_ratio, 1);
 
+
+%% Try rotate
+
+% change upper so that it does not intersect with lower
+verts_upper = verts_upper - repmat(vertical_axis' * -upper_bound, length(verts_upper), 1);
+pqp_deletemodel(obj_upper);
+obj_upper = pqp_createmodel(verts_upper', faces_upper');
+
+transMat = createRotation3dLineAngle(molar3line, pi / 360);
+intersects = pqp_intersect(obj_lower, obj_upper, transMat(1:3, 1:3), ...
+    transMat(1: 3, 4), 1);
 
 %% End
-pqp_deletemodel(obj_lower);
-pqp_deletemodel(obj_upper);
+%pqp_deletemodel(obj_lower);
+%pqp_deletemodel(obj_upper);
